48c53d3b0b7bc7bf3a5a7fcceb7f296e24c85b16
kzdiplomaproject2025 <diplomaproject00@gmail.com>
2025-06-02 21:19:50 +0500

    Update student_serializer.rb

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :second_name,:surname, :age, :description, :admission_number,:parents
+  attributes :id, :first_name, :last_name, :surname, :age, :description, :admission_number,:parents
   has_many :parents
 end
